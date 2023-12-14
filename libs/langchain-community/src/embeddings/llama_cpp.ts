0cb0d87d24be26a9b555911fa4aa5f394aa2025c
Nigel Daniels <nigel.daniels@me.com>
2023-12-14 11:47:54 -0800

    refactored embedding.

--- libs/langchain-community/src/embeddings/llama_cpp.ts
+++ libs/langchain-community/src/embeddings/llama_cpp.ts
@@ -49,55 +49,41 @@ export class LlamaCppEmbeddings extends Embeddings {
    * @param texts - An array of strings to generate embeddings for.
    * @returns A Promise that resolves to an array of embeddings.
    */
-  async embedDocuments(texts: string[]): Promise<number[][]> {
-    const tokensArray = [];
+   async embedDocuments(texts: string[]): Promise<number[][]> {
+     const tokensArray = [];
 
-    for (const text of texts) {
-      const encodings = await this.caller.call(
-        () =>
-          new Promise((resolve) => {
-            resolve(this._context.encode(text));
-          })
-      );
-      tokensArray.push(encodings);
-    }
+     for (const text of texts) {
+       const encodings = await this.caller.call(
+         () =>
+           new Promise((resolve) => {
+             resolve(this._context.encode(text));
+           })
+       );
+       tokensArray.push(encodings);
+     }
 
-    const embeddings: number[][] = [];
+     const embeddings: number[][] = [];
 
-    for (const tokens of tokensArray) {
-      const embedArray: number[] = [];
+     for (const tokens of tokensArray) {
+       const embedArray: number[] = [];
 
-      for (let i = 0; i < tokens.length; i += 1) {
-        const nToken: number = +tokens[i];
-        embedArray.push(nToken);
-      }
+       for (let i = 0; i < tokens.length; i += 1) {
+         const nToken: number = +tokens[i];
+         embedArray.push(nToken);
+       }
 
-      embeddings.push(embedArray);
-    }
+       embeddings.push(embedArray);
+     }
 
-    return embeddings;
-  }
+     return embeddings;
+   }
 
   /**
    * Generates an embedding for a single text.
    * @param text - A string to generate an embedding for.
    * @returns A Promise that resolves to an array of numbers representing the embedding.
    */
-  async embedQuery(text: string): Promise<number[]> {
-    const tokens: number[] = [];
-
-    const encodings = await this.caller.call(
-      () =>
-        new Promise((resolve) => {
-          resolve(this._context.encode(text));
-        })
-    );
-
-    for (let i = 0; i < encodings.length; i += 1) {
-      const token: number = +encodings[i];
-      tokens.push(token);
-    }
-
-    return tokens;
+  async embedQuery(document: string) {
+    return (await this.embedDocuments([document]))[0];
   }
 }
